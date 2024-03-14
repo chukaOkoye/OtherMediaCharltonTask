6e7ee2ff06d455f993d1dd497bcc5b116f729313
chukaOkoye <chuka93@hotmail.co.uk>
2024-03-14 17:35:00 +0000

    feat: adjust MatchListAdapter for binding conditionals

--- app/src/main/java/com/example/othermediacharlton/adapter/MatchListAdapter.kt
+++ app/src/main/java/com/example/othermediacharlton/adapter/MatchListAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.othermediacharlton.adapter
 
-import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,13 +13,15 @@ import com.bumptech.glide.Glide
 import com.example.othermediacharlton.R
 import com.example.othermediacharlton.data.model.FixturesDataModel
 import com.example.othermediacharlton.data.model.Match
+import java.util.Date
+import java.util.Locale
 
 class MatchListAdapter: RecyclerView.Adapter<MatchListAdapter.MyViewHolder>()   {
 
     var matchListData = ArrayList<Match>()
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchListAdapter.MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val inflater = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
         return MyViewHolder(inflater)
     }
@@ -28,7 +30,7 @@ class MatchListAdapter: RecyclerView.Adapter<MatchListAdapter.MyViewHolder>()   
         return matchListData.size
     }
 
-    override fun onBindViewHolder(holder: MatchListAdapter.MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bind(matchListData[position])
     }
 
@@ -41,21 +43,31 @@ class MatchListAdapter: RecyclerView.Adapter<MatchListAdapter.MyViewHolder>()   
 
     class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
 
-        val matchInfo = view.findViewById<TextView>(R.id.matchInfoDate)
-        val venueShortName = view.findViewById<TextView>(R.id.venueShortName)
-        val matchStatusIconPlayed = view.findViewById<ImageView>(R.id.matchStatusIconPlayed)
-        val homeTeam = view.findViewById<TextView>(R.id.homeTeam)
-        val homeTeamScore = view.findViewById<TextView>(R.id.homeTeamScore)
-        val awayTeam = view.findViewById<TextView>(R.id.awayTeam)
-        val awayTeamScore = view.findViewById<TextView>(R.id.awayTeamScore)
-        val opponentImagePlaceHolder = view.findViewById<ImageView>(R.id.opponentImagePlaceHolder)
-        val firstCalltoAction = view.findViewById<Button>(R.id.firstCalltoAction)
-        val secondCalltoAction = view.findViewById<Button>(R.id.secondCalltoAction)
+        val matchCentre = R.string.match_centre
+        val hospitality = R.string.hospitality
+        val buyTickets = R.string.buy_tickets
+        val report = R.string.report
+
+        private val resources = itemView.context.resources
+
+        private val matchInfo = view.findViewById<TextView>(R.id.matchInfoDate)
+        private val venueShortName = view.findViewById<TextView>(R.id.venueShortName)
+        private val matchStatusIconPlayed = view.findViewById<ImageView>(R.id.matchStatusIconPlayed)
+        private val homeTeam = view.findViewById<TextView>(R.id.homeTeam)
+        private val homeTeamScore = view.findViewById<TextView>(R.id.homeTeamScore)
+        private val awayTeam = view.findViewById<TextView>(R.id.awayTeam)
+        private val awayTeamScore = view.findViewById<TextView>(R.id.awayTeamScore)
+        private val opponentImagePlaceHolder = view.findViewById<ImageView>(R.id.opponentImagePlaceHolder)
+        private val firstCalltoAction = view.findViewById<Button>(R.id.firstCalltoAction)
+        private val secondCalltoAction = view.findViewById<Button>(R.id.secondCalltoAction)
 
 
          fun bind(data: Match){
-             matchInfo.text = data.matchInfo.date.toString()
+
+             val timestampInSeconds = formatDate(data.matchInfo.date.toLong())
+             matchInfo.text = timestampInSeconds
              venueShortName.text = data.matchInfo.venue.shortName
+
              if (data.liveData.matchStatus == "Played"){
                  matchStatusIconPlayed.setImageResource(R.drawable.icon_match_status_played)
              } else {
@@ -63,16 +75,16 @@ class MatchListAdapter: RecyclerView.Adapter<MatchListAdapter.MyViewHolder>()   
                  matchStatusIconPlayed.setImageResource(R.drawable.icon_match_status_fixture)
              }
              homeTeam.text = data.matchInfo.contestant[0].name
+             awayTeam.text = data.matchInfo.contestant[1].name
 
-             if (data.liveData.matchStatus != "Fixtures"){
+
+             if (data.liveData.matchStatus == "Played"){
                  homeTeamScore.text = data.liveData.homeScore.toString()
                  awayTeamScore.text = data.liveData.awayScore.toString()
              } else {
-                 homeTeamScore.text = ""
-                 awayTeamScore.text = "18:00".toString()
+                 homeTeamScore.text = resources.getString(R.string.empty_string)
+                 awayTeamScore.text = resources.getString(R.string.kick_off_time_default)
              }
-
-             awayTeam.text = data.matchInfo.contestant[1].name
 
 
              val url = data.matchInfo.contestant[0].crest.uri1x
@@ -84,24 +96,32 @@ class MatchListAdapter: RecyclerView.Adapter<MatchListAdapter.MyViewHolder>()   
              }
 
 
-             if (data.liveData.matchStatus == "Fixtures"){
+             if (data.liveData.matchStatus == "Played"){
                  firstCalltoAction.setBackgroundColor(
-                     ContextCompat.getColor(itemView.context, R.color.red))
-                 firstCalltoAction.text = "Buy Tickets"
-
-                 secondCalltoAction.setBackgroundColor(
                      ContextCompat.getColor(itemView.context, R.color.black))
-                 firstCalltoAction.text = "Hospitality"
+                 firstCalltoAction.setText(report)
+                 secondCalltoAction.setText(matchCentre)
+
              } else {
-                 firstCalltoAction.setBackgroundColor(
-                     ContextCompat.getColor(itemView.context, R.color.black))
-                 firstCalltoAction.text = "Report"
-
-                 secondCalltoAction.setBackgroundColor(
-                     ContextCompat.getColor(itemView.context, R.color.white))
-                 firstCalltoAction.text = "Match Centre"
+                 firstCalltoAction.setBackgroundResource(R.drawable.custom_button_shape)
+                 firstCalltoAction.setText(buyTickets)
+                 secondCalltoAction.setText(hospitality)
              }
 
          }
+
+        private fun formatDate(timestampInSeconds: Long): String {
+            // Convert Unix timestamp to milliseconds
+            val timestampInMillis = timestampInSeconds * 1000
+
+            // Create a Date object
+            val date = Date(timestampInMillis)
+
+            // Define the desired date format
+            val dateFormat = SimpleDateFormat("dd/MM/yy", Locale.getDefault())
+
+            // Format the date to the desired format
+            return dateFormat.format(date)
+        }
     }
 }
