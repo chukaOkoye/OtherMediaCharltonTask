9fdcee74ac554a64a261fe57cf82adb8c2fdcfec
chukaOkoye <chuka93@hotmail.co.uk>
2024-03-18 09:07:03 +0000

    feat: include test files

---
+++ app/src/test/java/com/example/othermediacharlton/FixturesRepositoryTest.kt
@@ -0,0 +1,10 @@
+package com.example.othermediacharlton
+
+class FixturesRepositoryTest {
+
+    /**
+     * In a production app, I would have tested the Repository interactions
+     * (the DI framework can help with the setup here)
+     */
+
+}
---
+++ app/src/test/java/com/example/othermediacharlton/MainActivityViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.example.othermediacharlton
+
+class MainActivityViewModelTest {
+
+/**
+ * In a production app, I would have tested the ViewModel interactions
+ * (the DI framework can help with the setup here)
+ */
+
+}
+
